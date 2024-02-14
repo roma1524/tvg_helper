e695068a80faa191db6820b601d40d211c2a6053
rmakulik <kulikr.js@mail.ru>
2024-02-14 22:12:41 +0300

    first commit

---
+++ conts.js
@@ -0,0 +1,13 @@
+const commands = `
+/tolls_for_roads - Оплата дорог
+/working_hours - Режим труда
+/contacts - Контакты
+/other - Прочее
+`
+
+const bd_text = [
+    [`<a href="https://www.eurovignettes.eu/portal/booking/booking?reset=true">Надо купить для водителя виньетку</a>`]
+]
+
+module.exports.commands = commands;
+module.exports.bd_text = bd_text;
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log('working...');
+const { Telegraf, Markup } = require("telegraf")
+require('dotenv').config()
+const text = require('./conts')
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+// bot.start((ctx) => ctx.reply(ctx.message));
+bot.start((ctx) => ctx.reply(`Привет ${ ctx.message.from.first_name ? ctx.message.from.first_name : 'bitch' }. Чтобы начать пользоваться ботом, нажми на "меню"`));
+bot.help((ctx) => ctx.reply(text.commands));
+
+bot.command('tolls_for_roads', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Оплата дорог</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Голландия', 't_btn_1'), Markup.button.callback('Дания', 't_btn_1'), Markup.button.callback('Швеция', 't_btn_1'), Markup.button.callback('Люксембург', 't_btn_1')],
+                [Markup.button.callback('Чехия', 't_btn_5'), Markup.button.callback('Польша', 't_btn_6')],
+                [Markup.button.callback('Швейцария', 't_btn_7'), Markup.button.callback('Словения', 't_btn_8')],
+                [Markup.button.callback('Словакия', 't_btn_9'), Markup.button.callback('Франция', 't_btn_10')]
+            ]
+        ))
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
+function addActionBot(name, src, argText) {
+    bot.action(name, async (ctx) => {
+        try {
+            await ctx.answerCbQuery(); // функция для обработки часов на кнопке
+
+            if ( src !== false) {
+                await ctx.replyWithPhoto({
+                    source: src
+                })
+            }
+
+            await ctx.replyWithHTML(argText, {
+                disable_web_page_preview: true
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+addActionBot('t_btn_1', false, text.bd_text[0][0])
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
