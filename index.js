4ee67df2a09580b098a8ee11b0b1cb18096b168a
rmakulik <kulikr.js@mail.ru>
2024-02-14 23:09:13 +0300

    menu

--- index.js
+++ index.js
@@ -22,6 +22,78 @@ bot.command('tolls_for_roads', async (ctx) => {
     }    
 })
 
+
+bot.command('working_hours', async (ctx) => {
+    try {
+        await ctx.replyWithHTML(`${text.estr_text[0][0]}`)
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
+bot.command('contacts', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Контакты</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Бухгалтерия', 'ct_btn_1')],
+                [Markup.button.callback('Менеджера', 'ct_btn_2')],
+                [Markup.button.callback('Диспетчер', 'ct_btn_3')],
+                [Markup.button.callback('Инструктор', 'ct_btn_4')],
+                [Markup.button.callback('Механик DAF', 'ct_btn_5'), Markup.button.callback('Механик MB', 'ct_btn_6')],
+                [Markup.button.callback('Персонал', 'ct_btn_7')],
+                [Markup.button.callback('Заменены водителей', 'ct_btn_8')],
+            ]
+        ))
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
+bot.command('docs', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Документы в рейсе</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Список документов', 'doc_btn_1')],
+                [Markup.button.callback('Заполнение CMR', 'doc_btn_2')],
+                [Markup.button.callback('Палетшайн', 'doc_btn_3')],
+                [Markup.button.callback('Европротокол', 'doc_btn_4')]
+            ]
+        ))
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
+bot.command('refueling', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Заправка</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('по Q8', 'fu_btn_1')],
+                [Markup.button.callback('по DKV', 'fu_btn_2')],
+                [Markup.button.callback('GlobalTank', 'fu_btn_3')],
+                [Markup.button.callback('VIADA', 'fu_btn_4')]
+            ]
+        ))
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
+bot.command('questions', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Основые вопросы в рейсе</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Слив топлива', 'q_btn_1')],
+                [Markup.button.callback('ДТП', 'q_btn_2')],
+                [Markup.button.callback('Повреждение груза', 'q_btn_3')],
+                [Markup.button.callback('Обмен паллет', 'q_btn_4')]
+            ]
+        ))
+    } catch (error) {
+        console.log(error);
+    }    
+})
+
 function addActionBot(name, src, argText) {
     bot.action(name, async (ctx) => {
         try {
